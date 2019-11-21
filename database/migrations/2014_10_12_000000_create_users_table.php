cf309aadb88871a9802bdbaeb1d73e16385ce57c
kkokwara2014 <kkokwara2014@gmail.com>
2019-11-21 19:58:49 +0100

    Creating models and migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('lastname');
+            $table->string('firstname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone');
             $table->string('password');
+            $table->string('userimage')->default('defaultuserimage.jpg');
             $table->rememberToken();
             $table->timestamps();
         });
