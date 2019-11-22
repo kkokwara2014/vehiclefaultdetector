a918cdec97d6e236ccf9de1102e483cf5c9f05fb
kkokwara2014 <kkokwara2014@gmail.com>
2019-11-22 03:45:28 +0100

    --- Auto-Git Commit ---

---
+++ app/Http/Controllers/SolutionreviewController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Solutionreview;
+use Illuminate\Http\Request;
+
+class SolutionreviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Solutionreview  $solutionreview
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Solutionreview $solutionreview)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Solutionreview  $solutionreview
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Solutionreview $solutionreview)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Solutionreview  $solutionreview
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Solutionreview $solutionreview)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Solutionreview  $solutionreview
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Solutionreview $solutionreview)
+    {
+        //
+    }
+}
