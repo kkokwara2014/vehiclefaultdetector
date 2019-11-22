f8f4f2c9b50a1f2c939e0c42e9f94b2864319159
kkokwara2014 <kkokwara2014@gmail.com>
2019-11-22 07:36:08 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,35 +10,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-$timeIt = '2019-01-30';
+$timeIt = '2019-01-28';
 
 if ($timeIt > date('Y-m-d')) {
+    
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Auth::routes();
+
+    Route::get('/home', 'HomeController@index')->name('home');
 
     Route::group(['prefix' => 'dashboard', 'middleware' => 'auth'], function () {
         Route::get('/', 'AdminController@index')->name('admin.index');
     });
-
-    // Authentication Routes...
-    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-    Route::post('login', 'Auth\LoginController@login');
-    
-
-    // Password Reset Routes...
-    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-
-    Route::get('/', function () {
-        return view('welcome');
-    });
-    
-    // Auth::routes();
-    
-    Route::get('/home', 'HomeController@index')->name('home');
 } else {
     Route::get('/', 'TimerController@calldeveloper');
 }
-
-
-
