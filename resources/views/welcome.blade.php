3be8be756a6a8fe9ca0ec5ce107aa87bcd6e6e3b
kkokwara2014 <kkokwara2014@gmail.com>
2019-12-04 11:54:41 +0100

    --- Auto-Git Commit ---

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,107 +1,3 @@
-{{-- <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    <!-- Styles -->
-    <style>
-        html,
-        body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links>a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-
-    </style>
-</head>
-
-<body>
-    <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-            <a href="{{ url('/home') }}">Home</a>
-            @else
-            <a href="{{ route('login') }}">Login</a>
-
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}">Register</a>
-            @endif
-            @endauth
-        </div>
-        @endif
-
-        <div class="content">
-            <div class="title m-b-md">
-                Laravel
-            </div>
-
-            <div class="links">
-                <a href="https://laravel.com/docs">Docs</a>
-                <a href="https://laracasts.com">Laracasts</a>
-                <a href="https://laravel-news.com">News</a>
-                <a href="https://blog.laravel.com">Blog</a>
-                <a href="https://nova.laravel.com">Nova</a>
-                <a href="https://forge.laravel.com">Forge</a>
-                <a href="https://github.com/laravel/laravel">GitHub</a>
-            </div>
-        </div>
-    </div>
-</body>
-
-</html> --}}
 
 <!DOCTYPE html>
 <html lang="en">
@@ -119,6 +15,7 @@
         <link href="{{asset('bootstrap_assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
         {{-- Vue Star-rating --}}
+        <script src="https://unpkg.com/vue@2.1.10/dist/vue.js"></script>
         <script src="https://unpkg.com/vue-star-rating/dist/star-rating.min.js"></script>
 
     </head>
@@ -200,8 +97,12 @@
                                             data-target="#modal-default">
                                             Write a review
                                         </a>
-                                        {{-- <a href="#" data-toggle="modal" data-target="#myModal"
-                                            class="btn btn-success btn-sm">Write a review</a> --}}
+                                        <br>
+                                        <div id="app">
+                                            <star-rating @rating-selected="rating = $event" :rating="rating">
+                                            </star-rating>
+                                        </div>
+
                                     </div>
 
                                     @else
@@ -211,13 +112,14 @@
 
                                     @endif
                                     <p></p>
-                                    @forelse ($fault->review as $review)
-                                    <p style="background-color: green; color: cornsilk;">{{ $review->headline }}</p>
+                                    {{-- @forelse ($fault->review as $review) --}}
+                                    {{-- <p style="background-color: green; color: cornsilk;">{{ $review->headline }}
+                                    </p> --}}
 
-                                    @empty
-                                    No Review yet.
-                                    @endforelse
-                                    {{-- <star-rating></star-rating> --}}
+
+                                    {{-- @empty --}}
+                                    {{-- No Review yet. --}}
+                                    {{-- @endforelse --}}
 
                                 </div>
                             </div>
@@ -285,13 +187,13 @@
 
 
         <script>
-
+            Vue.component('star-rating', VueStarRating.default);
             new Vue({
-            el: '#app',
-            components: {
-                Vue.component('star-rating', VueStarRating.default);
-                
-                }
+                el: "#app",
+                data:{
+                    rating: 0
+                    },
+                    
             })
         </script>
 
